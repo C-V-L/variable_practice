7f8e22451044fe87d021b32e537574f7908e42d6
Conner Van Loan <cdvanloan@gmail.com>
2022-11-01 12:57:39 -0600

    Adds variables and statements

--- variables.rb
+++ variables.rb
@@ -1,5 +1,29 @@
 # Variable Practice
 
 name = "Conner"
-hobbies = "climbing, bike, travel"
+hobbies = "climbing, biking, and traveling"
+age = 29
+favorite_color = "Green"
+number_of_pets = 0
+has_pets = false
+current_salary = 0.0
+
+puts "My name is #{name}"
+
+puts "#{name} is #{age} years old"
+
+puts "#{name} can be usually be found #{hobbies}"
+
+name = "Conner"
+name # "Conner"
+name = "CVL"
+name # "CVL"
+
+puts "Nicknames are sometimes better so I am now #{name}"
+
+
+
+
+
+
 
